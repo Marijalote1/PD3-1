99b014f2e609f4413c957fb6a83b6cd01d0e970e
Marijalote1 <marijalote1@gmail.com>
2026-03-23 10:21:17 +0200

    Add PowerShell script to display top 5 RAM processes

---
+++ 1_procesi.ps1
@@ -0,0 +1,21 @@
+$procesi = Get-Process | Where-Object { $_.ProcessName -ne "svchost" } | 
+
+    Sort-Object WorkingSet64 -Descending | 
+
+    Select-Object -First 5 
+
+  
+
+$kopējaisRAM = 0 
+
+  
+
+foreach ($p in $procesi) { 
+
+    $ramMB = [math]::Round($p.WorkingSet64 / 1MB, 2) 
+
+    Write-Output "$($p.ProcessName) - $ramMB MB" 
+
+    $kopējaisRAM += $ramMB 
+
+} 
